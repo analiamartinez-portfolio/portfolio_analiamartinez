22827e985fd67591a465dd05b37b37a1f0d0db81
analiamartinez-portfolio <analiambenavente@gmail.com>
2020-06-01 18:54:02 +0200

    Portfolio v2.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 (function () {
+
     const mobiles = document.querySelectorAll('.mobile-inertia')
+
     window.onscroll = () => {
-        document.querySelectorAll('.reveal').forEach(el => {
-            isInViewport(el) ? el.classList.add('visible') : el.classList.remove('visible')
-        })
+
+        winScroll = window.scrollY
+
+        const scrollButtons = document.querySelectorAll('.transicion');
+        for (let elm of scrollButtons) {
+            elm.onclick = e => {
+                e.preventDefault()
+                const href = elm.getAttribute('href');
+                document.querySelector(href).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
+
         if (isInViewport(document.querySelector('.features'))) {
             mobiles[0].style.transform = `translateX(${mobiles[0].getBoundingClientRect().top * .3}px)`
             mobiles[1].style.transform = `translateX(${mobiles[1].getBoundingClientRect().top * .2}px)`
@@ -15,4 +28,5 @@
         const horInView = (rect.left <= window.innerWidth) && ((rect.left + rect.width) >= 0)
         return (vertInView && horInView)
     }
+
 })();
